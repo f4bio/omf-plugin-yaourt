849f48fe353034e7a400377667e7a855117c7927
Fabio Tea <ft2011@gmail.com>
2016-06-04 10:35:55 +0200

    fix

--- functions/yamanallkeys.fish
+++ functions/yamanallkeys.fish
@@ -1,3 +1,3 @@
 function yaourtallkeys -d "Get all keys for developers and trusted users"
-  curl https://www.archlinux.org/developers/ https://www.archlinux.org/trustedusers/ | awk -F\" '(/pgp.mit.edu/) {sub(/.*search=0x/,"");print $1}'" | xargs yaourt-key --recv-keys
+  curl https://www.archlinux.org/developers/ https://www.archlinux.org/trustedusers/ | awk -F\" '(/pgp.mit.edu/) {sub(/.*search=0x/,"");print $1}' | xargs pacman-key --recv-keys
 end
--- functions/yamansignkeys.fish
+++ functions/yamansignkeys.fish
@@ -1,8 +1,8 @@
 function yaourtsignkeys -d "Allow user to sign keys"
   for key in $argv;
-    yaourt-key --recv-keys $key
-    yaourt-key --lsign-key $key
-    printf 'trust\n3\n' | sudo gpg --homedir /etc/yaourt.d/gnupg \
+    pacman-key --recv-keys $key
+    pacman-key --lsign-key $key
+    printf 'trust\n3\n' | sudo gpg --homedir /etc/pacman.d/gnupg \
         --no-permission-warning --command-fd 0 --edit-key $key
   end
 end
